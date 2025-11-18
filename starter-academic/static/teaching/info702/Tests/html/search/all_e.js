c4aa55aaa82483ee5a35e67f6af7f6c0bbb9aca9
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2025-11-18 10:53:17 +0100

    Update TP INFO702

--- starter-academic/static/teaching/info702/Tests/html/search/all_e.js
+++ starter-academic/static/teaching/info702/Tests/html/search/all_e.js
@@ -3,6 +3,7 @@ var searchData=
   ['figures_0',['6 - Des images, des figures',['../cpp_tp0.html#cpp_tp0_6',1,'']]],
   ['filtrage_20médian_1',['6 - Filtrage médian',['../cpp_tp1.html#cpp_tp1_6',1,'']]],
   ['filtrage_20par_20convolution_2',['8 - Rehaussement de contraste par filtrage par convolution',['../cpp_tp1.html#cpp_tp1_8',1,'']]],
-  ['format_20pgm_3',['5 - Import / Export en format PGM',['../cpp_tp1.html#cpp_tp1_5',1,'']]],
-  ['formes_20simples_4',['4 - Objets complexes par assemblage de formes simples',['../cpp_tp0.html#cpp_tp0_4',1,'']]]
+  ['finales_3',['Notes finales',['../cpp_tp1.html#cpp_tp1_11',1,'']]],
+  ['format_20pgm_4',['5 - Import / Export en format PGM',['../cpp_tp1.html#cpp_tp1_5',1,'']]],
+  ['formes_20simples_5',['4 - Objets complexes par assemblage de formes simples',['../cpp_tp0.html#cpp_tp0_4',1,'']]]
 ];
